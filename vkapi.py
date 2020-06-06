0b7ec73a4aee2d91d81831cdb04af3bddfcfd17d
Oleg Podryadov <oleg_podryadov@mail.ru>
2020-06-06 18:05:44 +0500

    upd code

--- vkapi.py
+++ vkapi.py
@@ -1,5 +1,5 @@
 import requests
-vk_token = 'e0685bbc592aa4142f9988669619e412ba30258616cd7650fc378e2c3512f37725504cdf9b0df7cc3e4b4'
+vk_token = '0af2d01ff90e1041bc135127d25aa6199e4c7ae4fe9baa719d025719ad6b799cdcb1284c1e33a9a6f8dbe'
 
 
 class User:
@@ -8,26 +8,34 @@ class User:
         self.token = token
         self.id_user = id_user
 
-    def get_params(self):
-        return {
-            'access_token': self.token,
-            'v': 5.107,
-            'method': 'friends.getMutual',
-            'source_uid': user1.id_user,
-            'target_uid': user2.id_user,
-        }
+    def __str__(self):
+        return f'https://vk.com/id{self.id_user}'
 
     def __and__(self, other):
-        params = self.get_params()
+        users = []
+        user_1_id = self.id_user
+        user_2_id = other.id_user
+        params = self.get_mutual(user_1_id, user_2_id)
         response = requests.get(
             'https://api.vk.com/method/friends.getMutual',
             params
         )
-        return response.json()['response']
+        for user_id in response.json()['response']:
+            new_user = User(user_id, vk_token)
+            print(new_user)
+            users.append(new_user)
+        return users
+
+    def get_mutual(self, usr1, usr2):
+        return {
+            'access_token': self.token,
+            'v': 5.107,
+            'method': 'friends.getMutual',
+            'source_uid': usr1,
+            'target_uid': usr2,
+        }
 
 
 user1 = User('23225635', vk_token)
 user2 = User('66121880', vk_token)
 mutual_user_list = user1 & user2
-for item in mutual_user_list:
-    print('https://vk.com/id' + str(item))
